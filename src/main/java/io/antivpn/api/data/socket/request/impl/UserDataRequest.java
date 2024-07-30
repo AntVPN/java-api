a0b4dfe2a1620de3a91049dd72449eb609929079
Martin R. O <me@snake.rip>
2024-07-30 20:50:08 -0300

    chore: Update UserDataRequest to include event parameter

--- src/main/java/io/antivpn/api/data/socket/request/impl/UserDataRequest.java
+++ src/main/java/io/antivpn/api/data/socket/request/impl/UserDataRequest.java
@@ -2,6 +2,7 @@ package io.antivpn.api.data.socket.request.impl;
 
 import io.antivpn.api.data.socket.request.Request;
 import io.antivpn.api.data.socket.request.RequestType;
+import io.antivpn.api.utils.Event;
 import lombok.Getter;
 
 @Getter
@@ -11,17 +12,17 @@ public class UserDataRequest extends Request {
     private final String address;
     private final String server;
     private final String version;
-    private final boolean connected;
+    private final String event;
     private final boolean premium;
 
-    public UserDataRequest(String username, String uniqueId, String version, String address, String server, boolean connected, boolean premium) {
+    public UserDataRequest(String username, String uniqueId, String version, String address, String server, Event event, boolean premium) {
         super(RequestType.USER_DATA);
         this.username = username;
         this.uniqueId = uniqueId;
         this.version = version;
         this.address = address;
         this.server = server;
-        this.connected = connected;
+        this.event = event.name();
         this.premium = premium;
     }
 }
