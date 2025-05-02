68bc53f20ed2d654af956cbac9cb79500b0ce201
Dilan Olivera <dilanolivera12@gmail.com>
2025-05-01 22:32:01 -0300

    feat: Add new settings

--- src/main/java/io/antivpn/api/data/socket/response/impl/SettingsResponse.java
+++ src/main/java/io/antivpn/api/data/socket/response/impl/SettingsResponse.java
@@ -3,15 +3,22 @@ package io.antivpn.api.data.socket.response.impl;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 @AllArgsConstructor
 public class SettingsResponse {
     private final int enabled;
     private final String kickMessage;
-    private final String logLevel;
+    private final String shieldMode;
+    private final Date date;
 
     public SettingsResponse() {
-        this(1, "§cVPN Detected!\n§cPlease disable your VPN and rejoin.\n§cIf you believe this is a mistake, please contact an administrator.", "FINE");
+        this(1,
+                "§cVPN Detected!\n§cPlease disable your VPN and rejoin.\n§cIf you believe this is a mistake, please contact an administrator.",
+                "§cShield is enabled!\n§cPlease wait a couple seconds before joining.\n§cIf you believe this is a mistake, please contact an administrator.",
+                new Date()
+        );
     }
 
     public boolean isEnabled() {
