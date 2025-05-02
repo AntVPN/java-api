97d4c0621b5c238c53e7cb0c0c4ee2b68893f8c6
Dilan Olivera <dilanolivera12@gmail.com>
2025-05-01 22:25:09 -0300

    feat: Add new response values

--- src/main/java/io/antivpn/api/data/socket/response/impl/CheckResponse.java
+++ src/main/java/io/antivpn/api/data/socket/response/impl/CheckResponse.java
@@ -1,13 +1,22 @@
 package io.antivpn.api.data.socket.response.impl;
 
+import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 
 @Data
 public class CheckResponse {
     private String uid;
+    private String username;
+    private String ip;
+    private String country;
     private boolean valid;
+    @SerializedName("is_attack")
+    private boolean isAttack;
 
     public String toString() {
-        return String.format("DataResponse(uid=%s, valid=%s)", uid, valid);
+        return String.format(
+                "DataResponse(uid=%s, username=%s, ip=%s, country=%s, valid=%s, is_attack=%s)",
+                this.uid, this.username, this.ip, this.country, this.valid, this.isAttack
+        );
     }
 }
